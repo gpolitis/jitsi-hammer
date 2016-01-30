1bd630e2120f7290144efd672ee44286d7cdbd51
George Politis <gp@jitsi.org>
2016-01-30 18:08:46 +0000

    Mavenizes the hammer.

--- jitsi-hammer.sh
+++ jitsi-hammer.sh
@@ -1,43 +1,17 @@
-#!/bin/bash
+#!/bin/sh -e
 
-if [ $# -lt 1 ] ; then #[ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ] || [ $# -lt 1 ] ; then
-    set -- "-help"
+REBUILD=true
+BOSH_URI=http://jitsi-meet.george.jitsi/http-bind
+ROOM_NAME=room1
+USERS_COUNT=3
+SC_HOME_DIR_NAME=.jitsi-hammer
+SC_HOME_DIR=~/$SC_HOME_DIR_NAME
+SRC_HOME=~/jitsi-hammer
+LOG_HOME=$SC_HOME_DIR/log
+VIDEO_RTPDUMP=$SRC_HOME/simulcast.rtpdump
+
+if $REBUILD ; then
+  mvn clean compile
 fi
 
-
-
-kernel="$(uname -s)"
-if [ $kernel == "Darwin" ] ; then
-	SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-	
-	architecture="mac"
-else
-	SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-	
-	if [ $kernel == "Linux" ] ; then
-		architecture="linux"
-	elif [ $kernel == "FreeBSD" ] ; then
-		architecture="freebsb"
-	else
-		echo "Unknown system : "$kernel
-		echo "Must be mac, linux, freebsb or windows"
-		exit 1
-	fi
-	
-	machine="$(uname -m)"
-	if [ "$machine" == "x86_64" ] || [ "$machine" == "amd64" ] ; then
-		architecture=$architecture"-64"
-	fi
-fi
-
-mainClass="org.jitsi.hammer.Main"
-classpath=$(JARS=($SCRIPT_DIR/jitsi-hammer.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*]}")
-libs="$SCRIPT_DIR/lib/native/$architecture"
-logging_config="$SCRIPT_DIR/lib/logging.properties"
-
-LD_LIBRARY_PATH=$libs:$LD_LIBRARY_PATH java  \
-  -Djava.library.path=$libs \
-  -Djava.util.logging.config.file=$logging_config \
-  -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=$SCRIPT_DIR \
-  -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=.jitsi-hammer \
-  -cp $classpath $mainClass $@
+exec mvn exec:java -Dexec.args="-BOSHuri $BOSH_URI -room $ROOM_NAME -users $USERS_COUNT -videortpdump $VIDEO_RTPDUMP" -Djava.net.preferIPv4Stack=true -Djava.library.path=$SRC_HOME/lib/native/linux-64 -Djava.util.logging.config.file=$SRC_HOME/lib/logging.properties -Dnet.java.sip.communicator.SC_HOME_DIR_NAME="$SC_HOME_DIR_NAME" 2>&1 | tee $LOG_HOME/output.log
